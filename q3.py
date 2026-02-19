656e2d6daeb6d6c5e7e20e9dc9cdf67ed63470b0
Shahriar Anan <110927484+Blaze-18@users.noreply.github.com>
2026-02-19 19:55:06 +0600

    Refactor bit plane merging functions and update main

--- q3.py
+++ q3.py
@@ -30,7 +30,7 @@ def get_bit_planes(image):
 # ---------------------------------
 # Function to merge bit planes
 # ---------------------------------
-def merge_bit_planes(bit_planes):
+def merge_all_planes(bit_planes):
     
     merged = np.zeros_like(bit_planes[0])
     
@@ -38,7 +38,20 @@ def merge_bit_planes(bit_planes):
         merged += (bit_planes[i] // 255) << i
     
     return merged
+    
 
+def merge_partial_plane(bit_planes, indices):
+
+    # Start with black image
+    merged = np.zeros_like(bit_planes[0])
+
+    # Add only selected planes
+    for i in indices:
+        merged += (bit_planes[i] // 255) << i
+
+    return merged
+
+    
 
 # ---------------------------------
 # Main function
@@ -51,15 +64,16 @@ def main():
     # Get bit planes
     bit_planes = get_bit_planes(image)
     
-    # Merge bit planes
-    merged_image = merge_bit_planes(bit_planes)
-    
-    
+    # Merge all bit planes
+    merged_image = merge_all_planes(bit_planes)
+    # Merge partial bit planes (0, 2, 4) and 1, 5, 6, 7
+    partial_1 = merge_partial_plane(bit_planes, [0, 2, 4])
+    partial_2 = merge_partial_plane(bit_planes, [1, 5, 6, 7])
     # Display results
     plt.figure(figsize=(12, 8))
     
     # Original
-    plt.subplot(3, 4, 1)
+    plt.subplot(4, 4, 1)
     plt.imshow(image, cmap='gray')
     plt.title("Original")
     plt.axis("off")
@@ -67,16 +81,26 @@ def main():
     
     # Show 8 bit planes
     for i in range(8):
-        plt.subplot(3, 4, i + 2)
+        plt.subplot(4, 4, i + 2)
         plt.imshow(bit_planes[i], cmap='gray')
         plt.title("Bit Plane " + str(i))
         plt.axis("off")
     
     
     # Merged image
-    plt.subplot(3, 4, 11)
+    plt.subplot(4, 4, 11)
     plt.imshow(merged_image, cmap='gray')
     plt.title("Merged")
+    plt.axis("off")
+    # Partial 1
+    plt.subplot(4, 4, 12)
+    plt.imshow(partial_1, cmap='gray')
+    plt.title("Merged: 0 2 4")
+    plt.axis("off")
+    # partial 2
+    plt.subplot(4, 4, 13)
+    plt.imshow(partial_2, cmap='gray')
+    plt.title("Merged: 1,5,6,7")
     plt.axis("off")
     
     
@@ -89,4 +113,3 @@ def main():
 # Run program
 if __name__ == "__main__":
     main()
-
